aa6821209cd8862c89617a248c92080664b71214
mubashshir <mubashshirislam201@gmail.com>
2025-08-25 01:54:32 +0530

    fix: correct product page params typing

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,12 +1,7 @@
-import { getProduct } from "@/lib/query"
+import { getProduct } from "@/lib/query";
 import Detail from "@/ui/details/detail";
-type Props = {
-    params:{id:string}
-}
-export default async function Product({params}:Props){
-    const {id} = params
-    const data = await getProduct({id});
-    return <>
-    <Detail data={data} />
-    </>
+
+export default async function Product({ params }: { params: { id: string } }) {
+  const data = await getProduct({ id: params.id });
+  return <Detail data={data} />;
 }
