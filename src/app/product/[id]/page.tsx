5ea62be674d96ee88cd205587def8f3587ac482a
mubashshir <mubashshirislam201@gmail.com>
2025-08-25 10:43:58 +0530

    Some new chages

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { getProduct } from "@/lib/query";
 import Detail from "@/ui/details/detail";
 
-export default async function Product({ params }: { params: { id: string } }) {
+export default async function Product({ params }: any) {
   const data = await getProduct({ id: params.id });
   return <Detail data={data} />;
 }
--- src/app/shop/[key]/[value]/page.tsx
+++ src/app/shop/[key]/[value]/page.tsx
@@ -1,7 +1,7 @@
 
 import { getItemsByAttribute } from "@/lib/query";
 import SpecifiedCatalog from "@/ui/catalog/specified";
-export default async function Shop({params}:{params:{key:"occasion" | "fabric" | "style"|"fit"|"color";value:string;}}){
+export default async function Shop({params}:{params:{key:"occasion" | "fabric" | "style"|"fit"|"color";value:any}}){
     const {key,value} = await params;
     console.log(typeof key)
     const data = await getItemsByAttribute({key,value})
