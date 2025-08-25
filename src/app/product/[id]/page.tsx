21d551e1d6bf2818341ae533a372b2e08b191ba6
Mubashshir Islam <mubashshirislam201@gmail.com>
2025-08-25 09:55:53 +0530

    Update page.tsx

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,12 @@
 import { getProduct } from "@/lib/query";
 import Detail from "@/ui/details/detail";
 
-export default async function Product({ params }: { params: { id: string } }) {
-  const data = await getProduct({ id: params.id });
+type ProductPageProps = {
+  params: { id: string | string[] };
+};
+
+export default async function Product({ params }: ProductPageProps) {
+  const id = Array.isArray(params.id) ? params.id[0] : params.id;
+  const data = await getProduct({ id });
   return <Detail data={data} />;
 }
